a58f647610b1a76c58d1d98c36a4e7685fb71535
brienze <brienze@gmail.com>
2017-09-15 00:20:45 -0300

    feat: Refactoring Created componentes Shelf and Shelves to modularize the existing code

--- src/App.js
+++ src/App.js
@@ -1,21 +1,35 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import * as BooksAPI from './BooksAPI'
+import ShelvesApp from './Shelves.js'
+import './App.css'
 
-class App extends Component {
+class BooksApp extends React.Component {
+  state = {
+    /**
+     * TODO: Instead of using this state variable to keep track of which page
+     * we're on, use the URL in the browser's address bar. This will ensure that
+     * users can use the browser's back and forward buttons to navigate between
+     * pages, as well as provide a good URL they can bookmark and share.
+     */
+      //showSearchPage: false
+      books : []
+    }
+
+
+  componentDidMount(){
+    BooksAPI.getAll().then((books) => {
+      this.setState({books})
+    })
+  }
+
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
+      /**
+      <ShelfApp name="Currently Reading" books={this.state.books.filter(f=>f.shelf=== 'currentlyReading')}/>
+      **/
+      <ShelvesApp name="Currently Reading" books={this.state.books}/>
+    )
   }
 }
 
-export default App;
+export default BooksApp
